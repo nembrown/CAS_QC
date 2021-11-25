d0333e8509884d9967f0ec57d8ef1fe65e2920d6
Brown <Norah.Brown@dfo-mpo.gc.ca>
2021-11-24 16:41:01 -0800

    added code for finding duplicates in wiretagcode, finding any codes in cwb but not wiretag and vice versa

--- CAS_exploration.R
+++ CAS_exploration.R
@@ -1,5 +1,9 @@
 library(odbc)
-
+library(tidyverse)
+remotes::install_git("https://github.com/Pacific-salmon-assess/tagFisheryMapping")
+library(tagFisheryMapping)
+library(dbplyr)
+library(janitor)
 
 #' Open CAS Database Connection
 #'
@@ -14,8 +18,7 @@ library(odbc)
 #'
 #' @export
 #'
-#'
-#'could the driver name be MS Access Database
+
 openCasConnection <- function(db_filename) {
   if (!requireNamespace("odbc", quietly = TRUE)) {
     stop("The package 'odbc' is needed for this function to work -
@@ -32,10 +35,47 @@ openCasConnection <- function(db_filename) {
   return(db_conn)
 }
 
-openCasConnection("CAMP2022BE.accdb")
-getwd()
-
 odbc::odbcListDrivers()
 
 casdb<-openCasConnection(file.path(getwd(), "CAMP2022BE.accdb"))
 dbListTables(casdb)
+
+#Functions are generally get something and then when you run it you need to assign to a df
+getCasFisheryTbl <- function(db_conn, stateprov_prefix) {
+  fishery_sql <-
+    paste0("SELECT Name as ctc_fishery_name, Description as ctc_fishery_description, CWDBFishery as psc_fishery_id, ",
+           "StateProvince as state_province, WaterType as water_type, Sector, Region, Area, Location, SubLocation, ",
+           "StartMonth as start_month, StartDay as start_day, EndMonth as end_month, EndDay as end_day, ",
+           "StartPeriod as start_period, EndPeriod as end_period, Stock ",
+           "FROM FisheryLookup x ",
+           "INNER JOIN (SELECT xc.Fishery, cc.Name, cc.Description FROM FisheryCFileFishery xc ",
+           "            INNER JOIN CFileFishery cc ON xc.CFileFishery = cc.Id) xc ON x.Fishery = xc.Fishery ",
+           "WHERE StateProvince = '", stateprov_prefix, "'")
+  
+  fishery_map_df <-
+    dbGetQuery(db_conn, fishery_sql) %>%
+    mutate(across(where(is.factor), as.character)) %>%
+    as_tibble() %>%
+    formatFisheryDef()
+  
+  return(fishery_map_df)
+}
+
+bc_fishery_table<-getCasFisheryTbl(casdb, "2")
+
+#translate_sql translates TO SQL from R in brackets
+translate_sql(merge(x, y))
+
+fisherylkup<-tbl(casdb, "FisheryLookup")
+wiretagcode<-tbl(casdb, "WireTagCode")
+cwdbrecovery<-tbl(casdb, "CWDBRecovery")
+
+wiretagcode_unq<-wiretagcode %>% as_tibble() %>% select(TagCode) %>% as.data.frame()
+cwdbrecovery_unq<-cwdbrecovery %>% as_tibble() %>% select(TagCode) %>% as.data.frame()
+  
+  
+wiretagcode %>% as_tibble() %>% get_dupes(TagCode)
+cwdbrecovery %>% as_tibble() %>% filter(TagCode %notin% wiretagcode_unq$TagCode)
+wiretagcode %>% as_tibble() %>% filter(TagCode %notin% cwdbrecovery_unq$TagCode)
+
+
